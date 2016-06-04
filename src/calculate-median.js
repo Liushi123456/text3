96f28fb8da68ffe8dc34209413174ea22cd30a23
Liushi123456 <10772859307@qq.com>
2016-06-04 15:39:12 +0800

    modify

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,11 +1,10 @@
 function calculate_median(arr) {
   //请勿改动函数名
-   var newarr = [];
-   var result;
+   var newarr=[];
    //取出数组中所有的偶数元素,放在newarr数组中
    for (var i = 0; i < arr.length; i++) {
      if (i%2===0) {
-       newarr=arr[i];
+        newarr=arr[i];
      }
    }
   //对newarr数组进行排序
@@ -22,13 +21,15 @@ function calculate_median(arr) {
      }
     }
   }
+
    //计算新数组的中位数
-    if(newarr.length%2===0){
-       result = (newarr[newarr.length/2]+newarr[newarr.length/2+1])/2;
+   var result=((newarr.length%2===0)?newarr[newarr.length/2]:(newarr[newarr.length/2]+newarr[newarr.length/2-1])/2);
+    /*if(newarr.length%2===0){
+      var result = (newarr[newarr.length/2]+newarr[newarr.length/2+1])/2;
     }else {
-       result=newarr[newarr.length/2];
-    }
+       result = newarr[newarr.length/2];
+    }*/
+   
     return result;
 }
-
 module.exports = calculate_median;
