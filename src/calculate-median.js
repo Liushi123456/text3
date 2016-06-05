e773a0c3ea9737e496dfbcb0c77e5fabd55f6289
Liushi123456 <10772859307@qq.com>
2016-06-05 16:31:54 +0800

    text3

--- src/calculate-median.js
+++ src/calculate-median.js
@@ -1,33 +1,26 @@
 function calculate_median(arr) {
   //请勿改动函数名
    var newarr=[];
+   var length;
    //取出数组中所有的偶数元素,放在newarr数组中
-   for (var i = 0; i < arr.length; i++) {
+   for ( var i = 0; i < arr.length; i++) {
      if ((i+1)%2===0) {
-        newarr=arr[i];
+        length=newarr.push(arr[i]);
      }
    }
   //对newarr数组进行排序
-   var length = newarr.length;
-   var i, j, temp;
+   var temp;
    for (i = 0; i < newarr.length; i++) {
-       for (j = 0; j < newarr.length-i-1; j++) {
-           if (newarr[j] > newarr[j + 1]) {
-               temp = newarr[j];
-               newarr[j] = newarr[j + 1];
-               newarr[j + 1] = temp;
+       for (j = i+1; j < newarr.length; j++) {
+           if (newarr[i] > newarr[j]) {
+               temp = newarr[i];
+               newarr[i] = newarr[j];
+               newarr[j] = temp;
            }
        }
    }
-
    //计算新数组的中位数
-   var result=(newarr.length%2===0)?(newarr[newarr.length/2]+newarr[newarr.length/2-1])/2:newarr[newarr.length/2];
-    /*if(newarr.length%2===0){
-      var result = (newarr[newarr.length/2]+newarr[newarr.length/2+1])/2;
-    }else {
-       result = newarr[newarr.length/2];
-    }*/
-   
-    return result;
+   var result=(length%2===0?(newarr[length/2]+newarr[length/2-1])/2:newarr[(length-1)/2]);
+   return result;
 }
 module.exports = calculate_median;
